7aa277599b5f470dd04e387d007c3c7617e46df5
Ravi Akhouri <techlead@bpcinfraprojects.com>
2026-02-16 01:02:30 +0530

    changes made for azure deployment

--- app.js
+++ app.js
@@ -92,6 +92,8 @@ app.use("/api/files/", require("./src/file/file.routes"));
 //--------ProtectedRoutes----------------------
 
 app.use(errorHandler);
-app.listen(3000, () => {
-  console.log(`server start at port no 3000`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
