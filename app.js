3db86daf520105f3bea7a3d6d810da4127ef72b3
techleadbpc <techlead@bpcinfraprojects.com>
2026-02-17 21:26:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -93,7 +93,9 @@ app.use("/api/files/", require("./src/file/file.routes"));
 
 app.use(errorHandler);
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
